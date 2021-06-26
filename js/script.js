51dedea0ee49884dfd0ec2930cecd83d009dc2e2
Nikolay Golub <kotkotkorshun@gmail.com>
2021-06-26 22:31:49 +0300

    Lesson1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//1
+
+let userAge=29;
+const userName="Nikolay";
+const userSurname="Golub";
+
+//2
+
+let newUser;
+let purchasesList;
+const userOnline=true;
+let salaryWorker;
+let coordinatesCursor;
