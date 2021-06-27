b6e5d81c3a74ac29856338df63d1f8c3f2d276fe
Nikolay Golub <kotkotkorshun@gmail.com>
2021-06-27 23:17:38 +0300

    l2

---
+++ homeworks/les2/js/script.js
@@ -0,0 +1,18 @@
+//1
+
+let userAge = 29;
+const userName = "Nikolay";
+const userSurname = "Golub";
+
+//2
+
+let newUser;
+let purchasesList;
+const userOnline = true;
+let salaryWorker;
+let coordinatesCursor;
+
+console.log("Helo world!");
+console.log("userAge:", userAge);
+console.log("userName:", userName);
+console.log("userSurname:", userSurname);
