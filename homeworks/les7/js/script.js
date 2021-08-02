c557abf56d657a3946bf687401c1fb05f72f45cd
Nikolay Golub <kotkotkorshun@gmail.com>
2021-08-02 20:41:25 +0300

    less 7

--- homeworks/les7/js/script.js
+++ homeworks/les7/js/script.js
@@ -3,17 +3,19 @@
 function TimeW() {
 
     const timeTable = {};
-    do{
+    let task;
+    let time;
+    do {
         task = prompt('what task you will do?');
-        if(task===null) {
+        if (task === null) {
             break;
         }
-        time = prompt ('when will you do the task?');
-if(time!==null) {
-    timeTable[time] = task;
-}
+        time = prompt('when will you do the task?');
+        if (time !== null) {
+            timeTable[time] = task;
+        }
 
-    } while(task!==null && time!==null );
+    } while (time !== null);
 
     return timeTable;
 }
@@ -22,4 +24,18 @@ console.log(TimeW());
 
 //2
 
+const salaries = {
+    John: "4300.00",
+    Ann: "5700.40",
+    Pete: "4900.95",
+};
+const getSalariesTotal = (salaries) => {
+    let total = 0;
+    for (let salary in salaries) {
+        total += Number(salaries[salary]);
+    }
+    return total.toFixed(2);
+}
+const totalResult = getSalariesTotal(salaries);
 
+console.log(totalResult);
