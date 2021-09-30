6e9bf37a1d475f0fdb2772a9d288f4350a42027a
Nikolay Golub <kotkotkorshun@gmail.com>
2021-10-01 01:34:43 +0300

    less 17

--- homeworks/les17/js/script.js
+++ homeworks/les17/js/script.js
@@ -1,50 +1,67 @@
 let intervalId = null;
 let timeMinute;
+let time = 0;
 
-const timer = () => {
+const timerContainer = document.querySelector('.timer_container');
+
+const displayCount = (timeMinute) => {
     let seconds = Math.trunc(timeMinute%60);
     let minutes = Math.trunc(timeMinute/60%60);
     let hours = Math.trunc(timeMinute/60/60%60);
-    let hoursDisplay = (hours < 10)? '0' + hours:hours;
-    let minutesDisplay = (minutes < 10)?'0' + minutes:minutes;
-    let secondsDisplay = (seconds < 10)? '0' + seconds:seconds;
 
-    timerContainer.textContent = hoursDisplay + ':' + minutesDisplay + ':' + secondsDisplay;
+    let displayHours = (hours < 10) ? '0' + hours : hours;
+    let displayMinutes = (minutes < 10) ? '0' + minutes : minutes;
+    let displaySeconds = (seconds < 10) ? '0' + seconds : seconds;
 
-    console.log(timerContainer.textContent);
+    timerContainer.textContent = displayHours + ':' + displayMinutes + ':' + displaySeconds;
 };
+
+displayCount(time);
 
 const start = (time) => {
-    if(!intervalId){
-        timeMinute = time;
-        return new Promise((resolve) => {
-            timer()
-            console.log(timeMinute)
+    timeMinute = --time;
+    return  new Promise((resolve) => {
+        if(!intervalId){
+            displayCount(timeMinute);
             intervalId = setInterval(() => {
-                timeMinute--
-                timer();
-                console.log(timeMinute)
+                if(timeMinute !== 0){
+                    timeMinute--;
+                }
+                displayCount(timeMinute);
                 if (timeMinute <= 0) {
                     clearInterval(intervalId);
-                    resolve()
+                    timeMinute = 0;
+                    resolve("Успех")
                 }
             }, 1000);
-        })
-    }
-
+        }
+    });
 };
-
 const pause = () => {
     clearInterval(intervalId);
     intervalId = null;
+    time = timeMinute
+};
+const reset = () => {
+    clearInterval(intervalId);
+    intervalId = null;
+    time = Number(prompt('Enter time'));
+    displayCount(time);
+    timerContainer.style = 'background: #e413d461; color: #05e200;'
+    textFin.style = 'display: none;'
 };
 
-const timerContainer = document.querySelector('.timer');
 const startBtn = document.querySelector('.start');
 const pauseBtn = document.querySelector('.pause');
-startBtn.addEventListener('click', start);
+const resetBtn = document.querySelector('.reset');
+const textFin = document.querySelector('.text');
+startBtn.addEventListener('click', () => {
+    if(time > 0 || timeMinute > 0){
+        start(time).then(() => {
+            timerContainer.style = 'background: red; color: white;';
+            textFin.style = 'display: inline-block'
+        });
+    }
+});
 pauseBtn.addEventListener('click', pause);
-
-start(5).then(() => {
-    console.log('Finisht timer!')
-})
+resetBtn.addEventListener('click', reset);
