b0c172670fff46e2cfd915118d4ac4aa48709f17
= <=>
2021-10-29 23:08:16 +0300

    less 21 upd

--- homeworks/les21/js/script.js
+++ homeworks/les21/js/script.js
@@ -1,25 +1,15 @@
-// Реализуйте мини-приложение SWAPI Board для отображения информмационных карточек о звёздных кораблях, планетах и сухопутном транспорте из вселенной Звёздных войн. В реализации вам поможет уже знакомое вам открытое АПИ (swapi).
+// Реализуйте мини-приложение SWAPI Board для отображения информмационных карточек о звёздных кораблях, планетах и сухопутном транспорте
+// из вселенной Звёздных войн. В реализации вам поможет уже знакомое вам открытое АПИ (swapi).
 //     Для реализации необходимо использовать ES6 Class.
-//     На странице должна присутвовать форма, в которой присутствует select для выбора типа необходимого вам объекта (звёздный корабль, сухопутное ТС или планета) и input, текстовое поле для ввода айди ресурса.
-//     При сабмите формы с выбраным типом и заполенным полем айди, отправляется запрос на сервер, и с полученными данными должна отрисоваться карточка на странице.
+//     На странице должна присутвовать форма, в которой присутствует select для выбора типа необходимого вам объекта (звёздный корабль,
+//     сухопутное ТС или планета) и input, текстовое поле для ввода айди ресурса.
+//     При сабмите формы с выбраным типом и заполенным полем айди, отправляется запрос на сервер, и с полученными данными должна отрисоваться
+//     карточка на странице.
 //     Для получения данных вам понадобятся следующие эндпоинты
 // https://swapi.dev/api/starships/${id} для кораблей
-//     https://swapi.dev/api/vehicles/${id} для сухопутного транспорта
-//         https://swapi.dev/api/planets/${id} для планет
-//             Карточки для каждого типа сущности должна отрисовать уникальные данные. Для звездных кораблей (starships) это:
-//
-//     название (name)
-// модель (model)
-// производитель (manufacturer)
-// максимальная скорость (max_atmosphering_speed) Для планет:
-//     название (name)
-// климат (climate)
-// поверхность (terrain)
-// население (population) Для сухопутного транспорта (vehicles):
-// название (name)
-// стоимость (cost_in_credits)
-// количество человек в команде (crew)
-// возможное количество пассажиров (passengers)
+// https://swapi.dev/api/vehicles/${id} для сухопутного транспорта
+// https://swapi.dev/api/planets/${id} для планет
+// Карточки для каждого типа сущности должна отрисовать уникальные данные.
 // Если неообходимого ресурса с переданным айди не существует, то должен быть показан alert с соответсвующим текстом.
 //
 //     Каждая карточка должна может быть удалена с доски. Для этого в ней должна присутствовать кнопка-крестик.
@@ -28,8 +18,58 @@
 //
 //     Сard - базовый класс для карточки, cодержит базовую логику отрисовки и удаления карточки.
 //     PlanetCard, StarshipCard и VehicleCard которые содержат в себе логику рендера необходимых полей для конкретного типа карточки
-// API - класс содержащий в себе логику работы с сервером. Должны быть реальзованы методы для получения каждого ресурса и метод для отправки запроса / обработки ошибок.
-//     Необязательное задание продвинутой сложности: При обновлении страницы / закрытии вкладки, карточки должны сохраняться. То есть, при повторном входе пользователь должен увидеть ту же доску с карточками, что и при последнем визите.
+// API - класс содержащий в себе логику работы с сервером. Должны быть реальзованы методы для получения каждого ресурса и метод для
+// отправки запроса / обработки ошибок.
+//     Необязательное задание продвинутой сложности: При обновлении страницы / закрытии вкладки, карточки должны сохраняться. То есть,
+//     при повторном входе пользователь должен увидеть ту же доску с карточками, что и при последнем визите.
+
+const BASE_URL = 'https://swapi.dev/api';
+const optionSelect = ['starships', 'vehicles', 'planets'];
+const containerForm = document.createElement('form');
+const selectElem = document.createElement('select');
+const inputElem = document.createElement('input');
+const cardCreateBtn = document.createElement('button');
+const optionElem = optionSelect.map((option) => `<option value="${option}"> ${option} </option>`).join('');
+
+cardCreateBtn.innerText = 'Add Card';
+inputElem.placeholder = 'ID';
+selectElem.innerHTML = optionElem;
+
+containerForm.append(selectElem, inputElem, cardCreateBtn);
+document.body.append(containerForm);
+
+cardCreateBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    let id = inputElem.value;
+    let select = selectElem.value;
+    let optionsFromAPI = {select, id};
+    return new API(optionsFromAPI).data();
+});
+class API {
+    constructor(option){
+        const {select, id} = option;
+        this.select = select;
+        this.id = id;
+        this.selectOption = async (select, id) => {
+            // /${select}/${id}
+            const request = await fetch(`${BASE_URL}/${select}`);
+            const response = await request.json();
+            console.log(response)
+            return response
+        };
+    }
+
+    data(){
+        // this.selectOption = async (select, id) => {
+        //     const request = await fetch(`${BASE_URL}/${select}/${id}`);
+        //     const response = await request.json();
+        //     console.log(response)
+        //     return response
+        // };
+        this.selectOption(this.select, this.id)
+    }
+}
+
 
 class Card {
     constructor(options) {
@@ -72,95 +112,43 @@ class Card {
     }
 }
 
-//card.show();
-
-class Input {
-    constructor() {
-        const inputElem = document.createElement('input')
-    }
-
-}
-
-class Form extends Card {
-    constructor(options) {
-        const {select, id, ...rest} = options;
-        super(rest);
-        this.options = options;
-        this.BASE_URL = 'https://swapi.dev/api';
-        this.selectOption = async (select, id) => {
-            const response = await fetch(`${this.BASE_URL}/${select}/${id}`);
-            return await response.json();
-        };
-        this.render()
-    }
-
-    render() {
-        // super.render();
-        const containerForm = document.createElement('form');
-        const selectElem = document.createElement('select');
-        const inputElem = document.createElement('input');
-        const cardCreateBtn = document.createElement('button');
-        const optionElem = this.options.map((option) => `<option value="${option}"> ${option} </option>`).join('');
-
-        // const selectOption = async (option, id) => {
-        //     const response = await fetch(`${BASE_URL}/${option}/${id}`);
-        //     return  await response.json();
-        // };
-
-        cardCreateBtn.innerText = 'close';
-        inputElem.placeholder = 'ID';
-        selectElem.innerHTML = optionElem;
-        // console.log(selectElem);
-
-        containerForm.append(selectElem, inputElem, cardCreateBtn);
-        document.body.append(containerForm);
-
-        cardCreateBtn.addEventListener('click', async (event) => {
-            event.preventDefault();
-            let id = inputElem.value;
-            let option = selectElem.value;
-            let res = {
-
-            };
-            this.selectOption(option, id)
-            // .then(response => {
-            //     console.log(response)
-            //     let {name, model, manufacturer, max_atmosphering_speed} = response;
-            //     res.title= name;
-            //     res.subtitle = model;
-            //     res.body = manufacturer;
-            //     res.footer = max_atmosphering_speed;
-            //     console.log(res)
-            //     return res
-            // });
-            //console.log(res)
-            // console.log(id);
-            // console.log(option);
-            const card = new Card({title:'sdf', fwdf:'sdf', fdeef:'sdf', fwesdf:'sdf'});
-            card.render();
-
-        })
-    }
-}
-
-const form = new Form(['starships', 'vehicles', 'planets']);
-
-// document.body.append(form)
-
 class Vehicles {
-    constructor(name) {
-
+    constructor(option){
+        // название (name)
+        // стоимость (cost_in_credits)
+        // количество человек в команде (crew)
+        // возможное количество пассажиров (passengers)
+        const {name, cost_in_credits, crew, passengers} = option;
+        this.title = name;
+        this.subtitle = cost_in_credits;
+        this.body = crew;
+        this.footer = passengers;
     }
 }
-
 class Starships {
-    constructor(name) {
-
+    constructor(option) {
+        // название (name)
+        // модель (model)
+        // производитель (manufacturer)
+        // максимальная скорость (max_atmosphering_speed)
+        const {name, model, manufacturer, max_atmosphering_speed} = option;
+        this.title = name;
+        this.subtitle = model;
+        this.body = manufacturer;
+        this.footer = max_atmosphering_speed;
     }
 }
 
 class Planet {
-    constructor(name) {
-
+    constructor(option) {
+        // название (name)
+        // климат (climate)
+        // поверхность (terrain)
+        // население (population)
+        const {name, climate, terrain, population} = option;
+        this.title = name;
+        this.subtitle = climate;
+        this.body = terrain;
+        this.footer = population;
     }
 }
